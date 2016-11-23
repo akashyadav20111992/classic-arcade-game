dfc2d5bf37a948570a5630df036464f74ecbc2c5
Akash Yadav <akashyadav20111992@gmail.com>
2016-11-23 20:06:52 +0530

    final arcade

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-frontend-nanodegree-arcade-game
-===============================
+Requirements and Instructions to run and play the game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+--> Requirements
+  * a web browser (Google Chrome recommended)
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+--> Instructions
+  * Play the game with Console display(in developer tools)
+  * Use arrow key or direction keys on keyboard to move the player
+  * On collision of player with enemy bug you lose the game and player comes to its initial position.
+  * On reaching water or blue lane, forwarding further you won the game and the player goes to its initial position.
--- js/app.js
+++ js/app.js
@@ -1,46 +1,108 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // The image/sprite for our enemies, this uses
+  // a helper to easily load images
+  this.sprite = 'images/enemy-bug.png';
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + this.speed * dt;
+  if(this.x > 505){
+    this.x = 0;
+  }
+  gamelose(this);
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y){
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt){
+  gamewon(this);
+}
 
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// this function handles the input provided by the user
+Player.prototype.handleInput = function(keyPressed){
+  if(keyPressed === 'left' && player.x > 0){
+    player.x = player.x - 100;
+  }
+  if(keyPressed === 'right' && player.x < 399){
+    player.x = player.x + 100;
+  }
+  if(keyPressed === 'up' && player.y > -15){
+    player.y = player.y - 85;
+  }
+  if(keyPressed === 'down' && player.y < 410){
+    player.y = player.y + 85;
+  }
+}
+
+// this function identifies the collision between
+//enemy and player
+var gamelose = function(bug){
+  if(player.y + 130 >= bug.y + 90
+    && player.x + 25 <= bug.x + 90
+    && player.y + 75 <= bug.y + 135
+    && player.x + 75 >= bug.x + 10){
+      player.x = 200;
+      player.y = 415;
+      console.log("You Lose");
+  }
+}
+
+// this function identifies the win of player
+var gamewon = function(player){
+  if(player.y <= -15){
+    player.x = 200;
+    player.y = 415;
+    console.log("You Win");
+  }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player(200, 415);
+for(var i=0;i<3;i++){
+  var enemy = new Enemy(0, ((Math.floor(Math.random() * (2 - 0+1)) + 0) * 85) + 60, Math.random() * 300);
+  allEnemies.push(enemy);
+}
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
